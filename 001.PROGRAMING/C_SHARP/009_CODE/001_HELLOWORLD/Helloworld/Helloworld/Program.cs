1283ed1f4bb591c4fec4806aeae7e73960f90f93
Jay Lee <g.jaehyeonlee@gmail.com>
2023-01-16 11:51:24 +0900

    [TL-20230116-002] C Sharp - Hello World

---
+++ 001.PROGRAMING/C_SHARP/009_CODE/001_HELLOWORLD/Helloworld/Helloworld/Program.cs
@@ -0,0 +1,14 @@
+﻿ namespace Helloworld
+{
+    internal class Program
+    {
+        // Entry Point 
+        // C# 프로그램은 반드시 어떤 함수에서 부터 시작해야한다.
+        // 그 어떤 함수가 Main 함수 이다. 
+        // arguments 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
